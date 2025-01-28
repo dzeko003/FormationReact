b88eda2c008c12e56c95576be60d5e6db960755b
dzeko003 <berenismassamba@gmail.com>
2025-01-28 22:48:56 +0100

    les champs controlés

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import { useState } from "react";
 
-
 function App() {
-  
-  const [person , setPerson] = useState({
-    name : "Berenis",
-    age : 0
-  })
+  const [value, setValue] = useState("");
+
+  const [checked, setCheked] = useState(true);
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
 
   const handleClick = () => {
-    setPerson({...person , age: person.age +1})
-  }
+    setCheked(!checked);
+  };
 
   return (
     <>
-      <p>Age : {person.age}</p>
-      <button onClick={handleClick}>Augmenter l'âge</button>
+      <form action="">
+        <textarea value={value} onChange={handleChange} />
+        <input type="checkbox" checked={checked} onClick={handleClick} />
+        <button disabled={!checked} type="submit">
+          Envoyer
+        </button>
+      </form>
     </>
   );
 }
