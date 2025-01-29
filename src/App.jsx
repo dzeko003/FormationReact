70705a51c0cfc4b590248116b03a7149763bf30a
dzeko003 <berenismassamba@gmail.com>
2025-01-29 23:37:34 +0100

    premier TP

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,88 @@
+import { Input } from "./components/forms/Input";
+import { Checkbox } from "./components/forms/Checkbox";
+import { ProductCategoryRow } from "./components/products/ProductCategoryRow";
+import { ProductRow } from "./components/products/productRow";
 import { useState } from "react";
 
+const PRODUCTS = [
+  { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+  { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+  { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+];
+
 function App() {
-  const [isTermAccepted , setIsTermAccepted] = useState(false);
+  const [showStockedOnly, setShowStockedOnly] = useState(false);
+  const [search , setSearch] =useState('');
 
-  return <form>
-      <CGUCheckbox checked={isTermAccepted} onChecked={setIsTermAccepted}  />
-      <button disabled={!isTermAccepted} >Envoyer le formulaire</button>
-  </form>;
-}
+  const visibleProducts = PRODUCTS.filter(product => {
+    if(showStockedOnly && !product.stocked){
+      return false;
+    }
 
-function CGUCheckbox({ checked, onChecked }) {
+    if(search && !product.name.includes(search)){
+      return false;
+    }
+
+    return true;
+  })
   return (
-    <div>
-      <label>
-        <input type="checkbox" checked={checked} onChange={(e) => onChecked(e.target.checked)} />
-        Accepter les conditions d'utilisation
-      </label>
+    <div className="container my-3">
+      <SearchBar
+        search={search}
+        onSearchChange={setSearch}
+        showStockedOnly={showStockedOnly}
+        onStockedOnlyChange={setShowStockedOnly}
+      />
+      <ProductTable products={visibleProducts} />
     </div>
   );
 }
 
+function SearchBar({showStockedOnly , onStockedOnlyChange , search , onSearchChange}) {
+  return (
+    <div>
+      <div className="mn-3">
+        <Input value={search} placeholder="Rechercher..." onChange={onSearchChange} />
+        <Checkbox
+          id="stocked"
+          checked={showStockedOnly}
+          onChange={onStockedOnlyChange}
+          label="N'afficher que des produits en stock"
+        />
+      </div>
+    </div>
+  );
+}
+
+function ProductTable({ products }) {
+  const rows = [];
+
+  let lastCategory = null;
+
+  for (let product of products) {
+
+    if (product.category != lastCategory) {
+      rows.push(
+        <ProductCategoryRow key={product.category} name={product.category} />
+      );
+    }
+
+    lastCategory = product.category;
+    rows.push(<ProductRow product={product} key={product.name} />);
+  }
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Prix</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
 export default App;
